ee1412ab9e9051b84be29cbc6035400df4a3448d
Aigerim <65478276+Aykosha@users.noreply.github.com>
2025-06-08 10:47:58 +0200

    Day 1: Learned print/input

--- python-challenge/day1.py
+++ python-challenge/day1.py
@@ -3,4 +3,5 @@
 name = input("What's your name? ")
 print(f"Hello, {name}! Welcome to Day 1 of 75-days-of-python challenge")
 
-#Completed Day1
+# Day 1 Motivation Boost!  
+print("Future you will thank present you for starting today. 💪")  
